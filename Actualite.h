2b587ba3d900e2ff9d23f623a06565b359cf5b88
feltaz <ouerfelli.moetaz@gmail.com>
2021-04-24 18:48:34 +0100

    setters and getters and constructor for class Date && constructor for class Actualite

--- Actualite.h
+++ Actualite.h
@@ -8,8 +8,12 @@ class Actualite
     Date dateActu;
     string globalOrLocal;
 public:
-    Actualite();
+    Actualite(string ="N/A",Date =(1,1,1111),string="N/A");
     ~Actualite(){}
     friend ostream& operator<<(ostream&,Actualite&);
     friend istream& operator>>(istream&, Actualite&);
 };
+Actualite::Actualite(string t,Date d,string gOrL):theme(t),dateActu(d),globalOrLocal(gOrL)
+{
+
+}
--- Date.h
+++ Date.h
@@ -1,9 +1,20 @@
 #pragma once
 #include <iostream>
 using namespace std;
-struct Date 
+class Date 
 {
     int jour;
     int mois;
-    int année;
+    int annee;
+public:
+    Date(int =1,int =1,int =1111);
+    ~Date(){}
+    int getJ(){return jour;}
+    int getM(){return mois;}
+    int getA(){return annee;}
+    void setJour(int j){jour=j;}
+    void setMois(int m){mois=m;}
+    void setAnnee(int a){annee=a;}
 };
+Date::Date(int j,int m,int a):jour(j),mois(m),annee(a)
+{}
