b5303ab11f37f7ee1fd33c1eff568eeb8d3d3f5a
feltaz <ouerfelli.moetaz@gmail.com>
2021-04-25 12:18:57 +0100

    implementing constructor,istream and ostream operators overloadedfor Date and Actualite class

--- Actualite.h
+++ Actualite.h
@@ -13,6 +13,24 @@ public:
     friend ostream& operator<<(ostream&,Actualite&);
     friend istream& operator>>(istream&, Actualite&);
 };
+ostream& operator<<(ostream& out,Actualite& A)
+{
+    out<<"\ntheme"<<A.theme<<"\n";
+    out<<"\ndate de l'Actualité:\n";
+    out<<A.dateActu;
+    out<<"\nglobal ou local?\n"<<A.globalOrLocal<<"\n";
+    return out;
+}
+istream& operator>>(istream& in,Actualite& A )
+{
+    cout<<"\nSaisir le theme: \n";
+    in>>A.theme;
+    cout<<"\nsaisir la date\n";
+    in>>A.dateActu;
+    cout<<"\nglobal ou local? \n";
+    in>>A.globalOrLocal;
+    return in;
+}
 Actualite::Actualite(string t,Date d,string gOrL):theme(t),dateActu(d),globalOrLocal(gOrL)
 {
 
--- Date.h
+++ Date.h
@@ -15,6 +15,22 @@ public:
     void setJour(int j){jour=j;}
     void setMois(int m){mois=m;}
     void setAnnee(int a){annee=a;}
+    friend ostream& operator<<(ostream&,Date&);
+    friend istream& operator>>(istream&,Date&);
 };
+istream& operator>>(istream& in,Date& d)
+{
+    cout<<"\nSaisir jour: \n";
+    in>>d.jour;
+    cout<<"\nSaisir mois: \n";
+    in>>d.mois;
+    cout<<"\nSaisir annee: \n";
+    in>>d.annee;
+}
+ostream& operator<<(ostream& out,Date& d)
+{
+    out<<"\nAffichage Date\n";
+    out<<"\n"<<d.jour<<"/"<<d.mois<<"/"<<d.annee<<"\n";
+}
 Date::Date(int j,int m,int a):jour(j),mois(m),annee(a)
 {}
