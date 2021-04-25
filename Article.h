5e632e3782c336946d313a6df732f7f9f81e368e
feltaz <ouerfelli.moetaz@gmail.com>
2021-04-25 12:35:48 +0100

    constructor and istream/ostream operator overloading for Article Class

--- Article.h
+++ Article.h
@@ -8,9 +8,28 @@ class Article
     string titre;
     string theme;
 public:
-    Article();
+    Article(long i=0,string t="n/a",string th="n/a"):idArticle(i),titre(t),theme(th){}
     ~Article(){}
     friend ostream& operator<<(ostream&,Article&);
     friend istream& operator>>(istream&,Article&);
-
 };
+ostream& operator<<(ostream& out, Article& a)
+{
+    out<<"\nidArticle: ";
+    out<<a.idArticle<<"\n";
+    out<<"\ntitre: ";
+    out<<a.titre<<"\n";
+    out<<"\ntheme: ";
+    out<<a.theme<<"\n";
+    return out;
+}
+istream& operator>>(istream& in, Article& a)
+{
+    cout<<"\nidArticle: ";
+    in>>a.idArticle;
+    cout<<"\ntitre: ";
+    in>>a.titre;
+    cout<<"\ntheme: ";
+    in>>a.theme;
+    return in;
+}
