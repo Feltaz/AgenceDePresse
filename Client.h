59ab93bea3080c09a8540a0ef7f9edd5719be3f3
feltaz <ouerfelli.moetaz@gmail.com>
2021-04-25 12:52:46 +0100

    constructors and overloading of istream,ostream& operators overloading of client and Journaliste Class

--- Client.h
+++ Client.h
@@ -7,8 +7,23 @@ class Client
     string prenom;
     string typeClient;
 public:
-    Client();
+    Client(string n="n/a",string p="n/a",string ty="n/a"):nom(n),prenom(p),typeClient(ty){}
     ~Client(){};
     friend ostream& operator<<(ostream&,Client&);
     friend istream& operator>>(istream&,Client&);
 };
+ostream& operator<<(ostream& out,Client& clt)
+{
+    out<<"\nnom et prenom Client: ";
+    out<<clt.nom<<" "<<clt.prenom<<"\n";
+    out<<"\ntypeClient: ";
+    out<<clt.typeClient<<"\n";
+    return out;
+}
+istream& operator>>(istream& in,Client& clt)
+{
+    in>>clt.nom;
+    in>>clt.prenom;
+    in>>clt.typeClient;
+    return in;
+}
--- Journaliste.h
+++ Journaliste.h
@@ -4,14 +4,35 @@ using namespace std;
 
 class Journaliste
 {
+    long idJournaliste;
     string prenom;
     string nom;
     string expertise;
     string paysDeService;
     string nationalité;
 public:
-    Journaliste();
+    Journaliste(long id=0,string p="n/a",string n="n/a",string exp="n/a",string pays="n/a",string nat="n/a"):idJournaliste(id),prenom(p),nom(n),expertise(exp),paysDeService(pays),nationalité(nat) {}
     ~Journaliste(){}
     friend ostream& operator<<(ostream&,Journaliste&);
-    friend istream& operator>>(ostream&,Journaliste&);
+    friend istream& operator>>(istream&,Journaliste&);
 };
+ostream& operator<<(ostream& out,Journaliste& j)
+{
+    out<<j.idJournaliste<<" ";
+    out<<j.prenom<<" ";
+    out<<""<<j.nom<<" ";
+    out<<""<<j.expertise<<" ";
+    out<<""<<j.paysDeService<<" ";
+    out<<""<<j.nationalité<<" ";
+    return out;
+}
+istream& operator>>(istream& in,Journaliste& j) 
+{
+    in>>j.idJournaliste;
+    in>>j.prenom;
+    in>>j.nom;
+    in>>j.expertise;
+    in>>j.paysDeService;
+    in>>j.nationalité;
+    return in;
+}
