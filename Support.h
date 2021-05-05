3bf0ea3befb02a3c0b8ce1f540d015bb9d10ed1e
feltaz <ouerfelli.moetaz@gmail.com>
2021-05-05 10:10:07 +0100

    implementing class Support

--- Support.h
+++ Support.h
@@ -6,8 +6,20 @@ class Support
     string nom;
     string adresse;
 public:
-    Support();
+    Support(string n="n/a",string add="n/a"):nom(n),adresse(add){}
     ~Support();
     friend ostream& operator<<(ostream&,Support&);
     friend istream& operator>>(istream&,Support&);
 };
+ostream& operator<<(ostream& out,Support&s)
+{
+    out<<s.nom;
+    out<<s.adresse;
+    return out;    
+}
+istream& operator>>(istream& in,Support&s)
+{
+    in>>s.nom;
+    in>>s.adresse;
+    return in;
+}
