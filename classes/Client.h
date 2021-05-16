f754abc7647292f68526572f12bba6df997b9ccb
feltaz <ouerfelli.moetaz@gmail.com>
2021-05-16 17:41:45 +0100

    adding id to clients class improving clarity in actualitefunctions.h

--- classes/Client.h
+++ classes/Client.h
@@ -3,17 +3,21 @@
 using namespace std;
 class Client
 {
+    long idClient;//id du client 
     string nom;//nom du client
     string prenom;//prenom du client
     string typeClient;//type de client indiv ou business
 public:
-    Client(string n="n/a",string p="n/a",string ty="n/a"):nom(n),prenom(p),typeClient(ty){}
+    Client(long id=0,string n="n/a",string p="n/a",string ty="n/a"):idClient(id),nom(n),prenom(p),typeClient(ty){}
     ~Client(){};
+    long getIdClient(){return idClient;}
     friend ostream& operator<<(ostream&,Client&);
     friend istream& operator>>(istream&,Client&);
 };
 ostream& operator<<(ostream& out,Client& clt)
 {
+    out<<"\nID du CLient:   ";
+    out<<clt.idClient<<"\n";
     out<<"\nnom et prenom Client: ";
     out<<clt.nom<<" "<<clt.prenom<<"\n";
     out<<"\ntypeClient: ";
@@ -22,8 +26,12 @@ ostream& operator<<(ostream& out,Client& clt)
 }
 istream& operator>>(istream& in,Client& clt)
 {
+    cout<<"\nID du CLient:   ";
+    in>>clt.idClient;
+    cout<<"\nnom et prenom Client: ";
     in>>clt.nom;
     in>>clt.prenom;
+    cout<<"\ntypeClient: ";
     in>>clt.typeClient;
     return in;
 }
